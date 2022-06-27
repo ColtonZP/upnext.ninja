055cf3723b7602c7813f8510a6122d90576849b1
Colton Pemberton <colton@coltonpemberton.com>
2022-06-26 19:23:21 -0700

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import './styles.css'
+import managerImage from './game-manager.png'
+
 export const App = () => {
   return (
-    <>
-      <h1>Up Next Ninja</h1>
-      <button type="button">Download now</button>
-    </>
+    <div className="container">
+      <h1>Up Next 🥷</h1>
+
+      <div className="game-manager">
+        <div className="info">
+          <h2>Game Manager</h2>
+
+          <p>An app that lets you track your game lists.</p>
+
+          <a href="https://gamesdemo.upnext.ninja">Try</a>
+          <button type="button">Download</button>
+          <img src={managerImage} alt="Game Manager" />
+        </div>
+      </div>
+    </div>
   )
 }
